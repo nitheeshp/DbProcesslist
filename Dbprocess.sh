b63ef5374e7af9410904de284ac348c287102cf8
nitheeshp <nitheeshp@outlook.com>
2014-02-23 13:09:04 +0530

    Update Dbprocess.sh

--- Dbprocess.sh
+++ Dbprocess.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#Show how many active connections there are to apache (httpd running on port 80)
+#Show how many active connections there are to mysql 
 active=$(netstat -an | grep 3306 | wc -l)
 echo $active
 number=30
 echo $number
-# If apache connection is more than 30 It will log into mysql and run the process list command and redirect output log file
+# If mysql connection is more than 30 It will log into mysql and run the process list command and redirect output log file
 if [ "$active" -ge "$number" ] 
 then
     mysql -u master -pyourpassword -hyourmysqlhostname -e "show full processlist" > /usr/local/nitheesh/dbprocess/processlist-`date +%F-%H:%M`.log
