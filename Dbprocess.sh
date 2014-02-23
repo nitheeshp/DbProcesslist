af12fa4d9645972e128fee34436cfdf111b1f0a0
nitheeshp <nitheeshp@outlook.com>
2014-02-23 13:05:24 +0530

    Adding dbprocess script

---
+++ Dbprocess.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Show how many active connections there are to apache (httpd running on port 80)
+active=$(netstat -an | grep 3306 | wc -l)
+echo $active
+number=30
+echo $number
+# If apache connection is more than 30 It will log into mysql and run the process list command and redirect output log file
+if [ "$active" -ge "$number" ] 
+then
+    mysql -u master -pyourpassword -hyourmysqlhostname -e "show full processlist" > /usr/local/nitheesh/dbprocess/processlist-`date +%F-%H:%M`.log
+else
+    echo "Number of connection is less"
+fi
